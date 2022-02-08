e223a2511742b54f6a0c07809db1b39c0688db4b
MoBaydoun <mbaydoun@my.bcit.ca>
2022-02-08 07:33:42 -0800

    i think i have  to use binary reader

--- Compression.cs
+++ Compression.cs
@@ -19,102 +19,6 @@ namespace ImageCompression
     class Compression
     {
 
-        public struct Pixel
-        {
-            public float R { get; set; }
-            public float G { get; set; }
-            public float B { get; set; }
-
-            public Pixel(float[] data)
-            {
-                R = data[0];
-                G = data[1];
-                B = data[2];
-            }
-
-            public Pixel(ConvertedPixel p)
-            {
-                R = p.Y + 1.402f * (p.Cr - 128);
-                G = p.Y - 0.344136f * (p.Cb - 128) - 0.714136f * (p.Cr - 128);
-                B = p.Y + 1.772f * (p.Cb - 128);
-            }
-
-            public override string ToString()
-            {
-                string s = $"R: {R}\nG: {G}\nB: {B}";
-                return s;
-            }
-
-
-            public static Pixel[,] DataToPixels(float[,] data)
-            {
-                int width = data.GetLength(0);
-                int height = data.GetLength(1);
-                Pixel[,] pixels = new Pixel[width, height / 3];
-                for (int y = 0; y < width; ++y)
-                {
-                    for (int x = 0; x < height; x += 3)
-                    {
-                        pixels[x, y / 3] = new Pixel(new float[] {
-                            data[x, y],
-                            data[x, y + 1],
-                            data[x, y + 2]});
-                    }
-                }
-                return pixels;
-            }
-        }
-
-        public struct ConvertedPixel
-        {
-            public float Y { get; set; }
-            public float Cb { get; set; }
-            public float Cr { get; set; }
-
-            public ConvertedPixel(Pixel p)
-            {
-                Y = 0 + (Constants.LUMINOSITY.X * p.R) + (Constants.LUMINOSITY.Y * p.G) + (Constants.LUMINOSITY.Z * p.B);
-                Cb = 128 - (Constants.BLUE_DIFF.X * p.R) - (Constants.BLUE_DIFF.Y * p.G) + (Constants.BLUE_DIFF.Z * p.B);
-                Cr = 128 + (Constants.RED_DIFF.X * p.R) - (Constants.RED_DIFF.Y * p.G) - (Constants.RED_DIFF.Z * p.B);
-            }
-
-            public override string ToString()
-            {
-                string s = $"Y: {Y}\nCb: {Cb}\nCr: {Cr}";
-                return s;
-            }
-
-            public static ConvertedPixel[,] ConvertPixels(Pixel[,] pixels)
-            {
-                int width = pixels.GetLength(0);
-                int height = pixels.GetLength(1);
-                ConvertedPixel[,] converted = new ConvertedPixel[width, height];
-                for (int i = 0; i < width; ++i)
-                {
-                    for (int j = 0; j < height; ++j)
-                    {
-                        converted[i, j] = new ConvertedPixel(pixels[i, j]);
-                    }
-                }
-                return converted;
-            }
-
-            public static byte[,] PixelsToData(ConvertedPixel[,] pixels)
-            {
-                byte[,] converted = new byte[pixels.GetLength(0), pixels.GetLength(1) * 3];
-                for (int i = 0; i < converted.GetLength(0); ++i)
-                {
-                    for (int j = 0; j < converted.GetLength(1); j += 3)
-                    {
-                        converted[i, j] = (byte)MathF.Round(pixels[i, j].Y);
-                        converted[i, j + 1] = (byte)MathF.Round(pixels[i, j].Cb);
-                        converted[i, j + 2] = (byte)MathF.Round(pixels[i, j].Cr);
-                    }
-                }
-                return converted;
-            }
-        }
-
         public Pixel[,] pixels { get; private set; }
         public Compression(Image img)
         {
@@ -310,7 +214,7 @@ namespace ImageCompression
 
             byte[] buffer = new byte[total];
             src.CopyPixels(buffer, stride, 0);
-
+            Trace.WriteLine(buffer.Length);
             float[,] data = new float[stride, stridenums];
 
             for (int y = 0; y < stridenums; ++y)
---
+++ Pixel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageCompression
+{
+    class Pixel
+    {
+        public float R { get; set; }
+        public float G { get; set; }
+        public float B { get; set; }
+
+        public Pixel(float[] data)
+        {
+            R = data[0];
+            G = data[1];
+            B = data[2];
+        }
+
+        public Pixel(ConvertedPixel p)
+        {
+            R = p.Y + 1.402f * (p.Cr - 128);
+            G = p.Y - 0.344136f * (p.Cb - 128) - 0.714136f * (p.Cr - 128);
+            B = p.Y + 1.772f * (p.Cb - 128);
+        }
+
+        public override string ToString()
+        {
+            string s = $"R: {R}\nG: {G}\nB: {B}";
+            return s;
+        }
+
+
+        public static Pixel[,] DataToPixels(float[,] data)
+        {
+            int width = data.GetLength(0);
+            int height = data.GetLength(1);
+            Pixel[,] pixels = new Pixel[width, height / 3];
+            for (int y = 0; y < width; ++y)
+            {
+                for (int x = 0; x < height; x += 3)
+                {
+                    pixels[x, y / 3] = new Pixel(new float[] {
+                            data[x, y],
+                            data[x, y + 1],
+                            data[x, y + 2]});
+                }
+            }
+            return pixels;
+
+        }
+    }
+}
